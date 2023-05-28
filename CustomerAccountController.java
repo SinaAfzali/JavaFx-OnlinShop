7d5296777cc871f223ef184de6a1196ef7fd2ae1
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-05-28 01:15:48 -1200

    this is the customer account

---
+++ CustomerAccountController.java
@@ -0,0 +1,92 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private Label moneyField;
+
+    @FXML
+    private TextField passField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField ruleField;
+
+    @FXML
+    private TextField userField;
+
+    @FXML
+    void setCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setChangeInformation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setDiscountCode(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setEmail(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setFirstName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setLastName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setMoney(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setNumber(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setPass(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setRecords(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setRule(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setUser(ActionEvent event) {
+
+    }
+
+}
