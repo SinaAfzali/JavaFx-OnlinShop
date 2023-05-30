79d2bdfafb64cd4eb1c7d734f23e6a7050499292
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-05-29 18:18:47 -1200

    forget controller
    this class is the part of the program that when the user forgot his/her password and wants a new one.

---
+++ ForgetController.java
@@ -0,0 +1,34 @@
+package com.example.OnlineShop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private TextField setCode;
+
+    @FXML
+    private TextField setPass;
+
+    @FXML
+    private TextField useEmail;
+
+    @FXML
+    void refresh(MouseEvent event) {
+
+    }
+
+    @FXML
+    void setConfirm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setReturn(MouseEvent event) {
+
+    }
+
+}
