b6a3da97c2c37d3be7fa4fab67fa61ca5e4677a8
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-05-30 08:11:00 -1200

    the signin page for new users

---
+++ SigninController.java
@@ -0,0 +1,49 @@
+package com.example.OnlineShop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField confirmCodeField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private Label setCodeSend;
+
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    void customerButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellerButton(ActionEvent event) {
+
+    }
+
+}
