c949964506a139e8d49122bc74292270f27a7ef7
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-05-31 18:17:08 -1200

    Add files via upload

---
+++ DiscountCodeController.java
@@ -0,0 +1,29 @@
+package com.example.OnlineShop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private Label discountCodeField;
+
+    @FXML
+    private ImageView discountLogo;
+
+    @FXML
+    private Label discountMoney;
+
+    @FXML
+    void backIcon(MouseEvent event) {
+
+    }
+
+    @FXML
+    void discountCopy(MouseEvent event) {
+
+    }
+
+}
