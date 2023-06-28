407d1e09db6b47890ca02ade5840fa1099617a50
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-06-28 06:45:51 -1200

    the new product page

---
+++ productController.java
@@ -0,0 +1,55 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button point1;
+
+    @FXML
+    private Button point2;
+
+    @FXML
+    private Button point3;
+
+    @FXML
+    private Button point4;
+
+    @FXML
+    private Button point5;
+
+    @FXML
+    private TextArea theReciveBox;
+
+    @FXML
+    private TextArea theSendBox;
+
+    @FXML
+    void setBack(MouseEvent event) {
+
+    }
+
+    @FXML
+    void setDecreas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setIncreas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setPoint(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setSend(ActionEvent event) {
+
+    }
+
+}
