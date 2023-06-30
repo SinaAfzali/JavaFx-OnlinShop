746c886b63782c130269bb0a6b8e336bb754c20f
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-06-30 10:26:02 -1200

    the history about selling and buying

---
+++ SavabeghKharidController.java
@@ -0,0 +1,52 @@
+package com.example.OnlineShop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class SavabeghKharidController {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private Label addressLabel;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private Pane infoPane;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private TextField namefield;
+
+    @FXML
+    private Label setTitle;
+
+    @FXML
+    private AnchorPane warehousePane;
+
+    @FXML
+    void back(MouseEvent event) {
+
+    }
+
+    @FXML
+    void createButton(ActionEvent event) {
+
+    }
+
+}
