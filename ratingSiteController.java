a7d40114e0db33148b86765329e11ca98df9686c
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-07-03 01:17:02 -1200

    rating the hole shop

---
+++ ratingSiteController.java
@@ -0,0 +1,39 @@
+package com.example.OnlineShop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class ratingSiteController {
+
+    @FXML
+    void confirmPoint(ActionEvent event) {
+
+    }
+
+    @FXML
+    void p1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void p2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void p3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void p4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void p5(MouseEvent event) {
+
+    }
+
+}
