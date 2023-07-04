47a043b65b4e7ffd126095645151482d1171c411
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-07-04 07:34:18 -1200

    the customer part for auction

---
+++ AuctionCustomerController.java
@@ -0,0 +1,33 @@
+package com.example.OnlineShop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AuctionCustomerController {
+
+    @FXML
+    private TextField setCustomerPrice;
+
+    @FXML
+    private Label setName;
+
+    @FXML
+    private Label setPrice;
+
+    @FXML
+    private Label setSeller;
+
+    @FXML
+    void backIcon(MouseEvent event) {
+
+    }
+
+    @FXML
+    void setPay(ActionEvent event) {
+
+    }
+
+}
