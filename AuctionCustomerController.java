f484d20fce0f8f4492d945b3637f47f363017527
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-07-04 10:53:41 -1200

    new auction customer

--- AuctionCustomerController.java
+++ AuctionCustomerController.java
@@ -2,11 +2,19 @@ package com.example.OnlineShop;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class AuctionCustomerController {
+
+    @FXML
+    private Button goToProductPage;
+
+    @FXML
+    private Label numberOfProduct;
 
     @FXML
     private TextField setCustomerPrice;
@@ -15,10 +23,26 @@ public class AuctionCustomerController {
     private Label setName;
 
     @FXML
+    private ImageView setPhoto;
+
+    @FXML
     private Label setPrice;
 
     @FXML
     private Label setSeller;
+
+    @FXML
+    private Label setTheGreatestPrice;
+
+    @FXML
+    void Decrease(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Increase(ActionEvent event) {
+
+    }
 
     @FXML
     void backIcon(MouseEvent event) {
