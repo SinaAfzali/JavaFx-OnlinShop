d53118ed38306432b0bcaf48ca3b0283e7d6c9e7
Sasan Nikjoo <123094930+83Sasan04@users.noreply.github.com>
2023-07-04 21:10:23 -1200

    the seller products

---
+++ MyProductsController.java
@@ -0,0 +1,21 @@
+package com.example.OnlineShop;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class CartController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    void backIcon(MouseEvent event) {
+
+    }
+
+}
